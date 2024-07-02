315aea081f9c31d56eb0dad17e04704b2555d69b
Axel Leos <rleos1802@outlook.com>
2024-07-02 11:16:52 -0600

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>counter: {{ counter }}</h3>
+
+  <button (click)="increaseBy(-1)">-1</button>
+  <button (click)="resetBy()">Reset</button>
+  <button (click)="increaseBy(1)">+1</button>
+
+  `
+})
+export class CounterComponent {
+  public title:string = 'Hi everyone';
+  public counter:number = 10;
+  // private initialCounter = 10;
+
+  increaseBy(value: number):void{
+    this.counter +=value;
+  }
+  resetBy():void{
+    this.counter = 10  /* this.initialCounter */
+  }
+}
